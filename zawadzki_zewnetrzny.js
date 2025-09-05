1b5aef7973dbb6db5643f9d6423ab96b9174ce88
Borcik123 <156583537+Borcik123@users.noreply.github.com>
2025-09-05 13:54:39 +0200

    Update zawadzki_zewnetrzny.js

--- zawadzki_zewnetrzny.js
+++ zawadzki_zewnetrzny.js
@@ -1,2 +1,2 @@
-document.write("ostatnia modyfikacja strony".fontcolor("purple").fontsize(7)+"<br>"); 
+document.write("ostatnia modyfikacja strony".fontcolor("silver").fontsize(6 )+"<br>"); 
 document.write(document.lastModified); 
